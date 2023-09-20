f2a5e17a3e1af03d965724a94fb0e31aa9324f7c
Vfivew <hitori555111555@yahoo.com>
2023-09-20 10:42:05 +0300

    vercel

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,20 +1,20 @@
-import { Link } from 'react-router-dom';
-import { Login } from '../components/Auth/Login';
+  import { Link } from 'react-router-dom';
+  import { Login } from '../components/Auth/Login';
 
-const LoginPage: React.FC = () => {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="flex flex-col items-center">
-        <Login />
-        <Link
-          className="base-btn mt-4"
-          to="/register"
-        >
-          Register
-        </Link>
+  const LoginPage: React.FC = () => {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <div className="flex flex-col items-center">
+          <Login />
+          <Link
+            className="base-btn mt-4"
+            to="/register"
+          >
+            Register
+          </Link>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
-export default LoginPage;
+  export default LoginPage;
